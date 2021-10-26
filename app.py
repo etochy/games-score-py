a9298230bca1dcf3785f618003fb2c87f1222af9
Esteban LAUNAY <esteban.launay@sii.fr>
2021-10-26 11:08:53 +0200

    scores in json file

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
 import sys
-from flask import Flask, json, request, jsonify
+from flask import Flask, request, jsonify
+import json
+import numpy as np
 
 app = Flask(__name__)
+
+max_size_data = 500
 
 @app.route("/")
 def home():
     return "Welcome!Here you are in IHM adaptative API prediction."
 
+# Save new score
 @app.route('/asteroid-scores', methods=['POST'])
 def saveScore():
+    scores = loadJson()
     data = request.json
-    return "coucou"
+    print(data)
+    scores.append(data)
+    scores.sort(reverse=True, key=sortScores)
+
+    #remove data if necessary
+    while len(scores) > max_size_data:
+        scores.pop()
+
+    saveJson(scores)
+
+    return jsonify(scores)
+
 
 @app.route('/asteroid-scores', methods=['GET'])
 def getScores():
-    return ""
+    scores = loadJson()
+    return jsonify(scores)
+
+def loadJson():
+    jsonfile = open('./scores.json', 'r')
+    new_scores = json.load(jsonfile)
+    jsonfile.close()
+    return new_scores
+
+def saveJson(scores):
+    jsonfile = open('./scores.json', 'w')
+    json.dump(scores, jsonfile)
+    jsonfile.close()
+    return
+
+def sortScores(e):
+  return e['score']
 
 if __name__ == '__main__':
     try:
